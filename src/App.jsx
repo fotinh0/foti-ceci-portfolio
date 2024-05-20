2cf6965ec7fe763caa060dc13c0421a9725c2eee
Foti Ceci <foti.ceci@gmail.com>
2024-05-20 11:12:11 -0400

    implement google analyics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import "./App.css";
 import Homepage from "./components/Homepage";
+import ReactGA from "react-ga";
+
+// Initialize Google Analytics
+ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
 
 function App() {
+  useEffect(() => {
+    // Track the initial page load
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <>
       <Homepage />
